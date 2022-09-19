1d41bfa894b3046d743c1be4d1bfb4d02596e2fe
Jesus <jesuspl@tutanota.com>
2022-09-19 23:27:04 +0200

    Adding fullscreen mode

--- app/src/main/java/com/jesuspinar/webviewer/MainActivity.java
+++ app/src/main/java/com/jesuspinar/webviewer/MainActivity.java
@@ -2,8 +2,12 @@ package com.jesuspinar.webviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private static final String HOME_WEB = "https://duckduckgo.com";
@@ -12,13 +16,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Hides action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
+        setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         //Default url
         webView.loadUrl(HOME_WEB);
-
     }
     //Go back action
     @Override
